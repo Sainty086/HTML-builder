091bcaa15972908639f327374b5a7eea1e64948d
Sainty086 <Sainty086@gmail.com>
2025-01-21 00:49:27 +0300

    feat: solve 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let path = require('node:path');
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathFolder, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(file.parentPath, file.name);
+        const fileName = path.basename(filePath, path.extname(filePath));
+        const fileExt = path.extname(filePath).toString().replace('.', '');
+        fs.stat(filePath, (err, stats) => {
+          if (err) console.log(err);
+          else {
+            const fileSize = stats.size;
+            console.log(`${fileName} - ${fileExt} - ${fileSize}b`);
+          }
+        });
+      }
+    });
+  }
+});
