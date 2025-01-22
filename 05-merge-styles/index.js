3eee25ecc5833a7bb1e17f3cd452cb5d785328d9
Sainty086 <Sainty086@gmail.com>
2025-01-22 13:24:52 +0300

    feat: solve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+let fs = require('fs');
+let path = require('node:path');
+const sourceFolder = path.join(__dirname, 'styles');
+const bundleFolder = path.join(__dirname, 'project-dist');
+const bundleFile = path.join(bundleFolder, 'bundle.css');
+
+const wrStream = fs.createWriteStream(bundleFile);
+
+fs.readdir(sourceFolder, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      const filePath = path.join(file.parentPath, file.name);
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        const reader = fs.createReadStream(filePath);
+        reader.on('data', function (chunk) {
+          wrStream.write(chunk);
+        });
+      }
+    });
+  }
+});
